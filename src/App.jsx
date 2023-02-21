3fa6fa8dd3ceb0f4cb34061ddb880728d0ab90ed
Sepehr Hashemi <sepehrhashemimamaquani1996@gmail.com>
2023-02-21 18:06:29 +0330

    Start The New React Project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex justify-center align-middle text-3xl font-bold underline bg-gradient-to-r from-cyan-300 to-blue-500">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
